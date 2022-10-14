687fe472723434619182a397b48018b73099b33c
Dipo122 <84640848+Dipo122@users.noreply.github.com>
2022-10-14 21:11:45 +0200

    Inizio codice

--- SScy/SScy/Form1.Designer.cs
+++ SScy/SScy/Form1.Designer.cs
@@ -28,21 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.id_txbx = new System.Windows.Forms.TextBox();
+            this.codice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(316, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MENU";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button1.Location = new System.Drawing.Point(224, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(294, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RICHIEDI LA CONNESSIONE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-253, 72);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(1499, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "_________________________________________________________________________________" +
+    "__________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(354, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SSCY";
+            // 
+            // id_txbx
+            // 
+            this.id_txbx.Location = new System.Drawing.Point(253, 182);
+            this.id_txbx.Name = "id_txbx";
+            this.id_txbx.Size = new System.Drawing.Size(241, 20);
+            this.id_txbx.TabIndex = 4;
+            // 
+            // codice
+            // 
+            this.codice.AutoSize = true;
+            this.codice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codice.ForeColor = System.Drawing.Color.Yellow;
+            this.codice.Location = new System.Drawing.Point(303, 366);
+            this.codice.Name = "codice";
+            this.codice.Size = new System.Drawing.Size(138, 24);
+            this.codice.TabIndex = 5;
+            this.codice.Text = "000000-00000";
+            this.codice.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(339, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "SScy";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ITALIANO",
+            "ENGLISH"});
+            this.comboBox1.Location = new System.Drawing.Point(676, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.codice);
+            this.Controls.Add(this.id_txbx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "home";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox id_txbx;
+        private System.Windows.Forms.Label codice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- SScy/SScy/Form1.cs
+++ SScy/SScy/Form1.cs
@@ -7,22 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SScy.Traslating; // Using Translation system
 
 namespace SScy
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-            // Ciao
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // PROVA NOOB
 
-            // BELLA
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Convert.ToString(id_txbx.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "ENGLISH")
+            {
+                button1.Text = "CONNECTION REQUEST";
+            }
+            else
+            {
+                button1.Text = "RICHIEDI LA CONNESSIONE";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(codice.Text);
+            if (comboBox1.Text == "ENGLISH")
+            {
+                MessageBox.Show("Code copied");
+            }
+            else
+            {
+                MessageBox.Show("Codice Copiato");
+            }
         }
     }
 }
+
+       
